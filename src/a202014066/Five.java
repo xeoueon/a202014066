abd0d54babd037da3f2b2e0924e9ddd85747513a
Kim, Seoyeon <ssyy9166@gamil.com>
2024-03-27 01:42:28 +0900

    five

---
+++ src/a202014066/Five.java
@@ -0,0 +1,8 @@
+package a202014066;
+
+public class Five {
+
+  public static void print() {
+    System.out.println("five");
+  }
+}
--- src/a202014066/Main.java
+++ src/a202014066/Main.java
@@ -3,6 +3,7 @@ package a202014066;
 public class Main {
 
   public static void main(String[] args) {
+	Five.print();
     Two.print();
     System.out.println("one 202014066");
     Three.print();
